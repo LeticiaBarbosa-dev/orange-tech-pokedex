071cf19b61322d72f6f0783b4abe20447da4132e
LeticiaBarbosa-dev <leticiapereiraab@gmail.com>
2022-11-27 16:44:00 -0300

    feat(darkmode): adicionando dark mode e favicon

--- assets/js/main.js
+++ assets/js/main.js
@@ -47,3 +47,35 @@ loadMoreButton.addEventListener("click", () => {
     loadPokemonItems(offset, limit);
   }
 });
+
+// Dark Mode
+const body = document.querySelector('body');
+const content = body.querySelector(".content");
+const checkbox = document.querySelector('input[name="theme"]');
+
+const getStyle = (element, style) => window.getComputedStyle(element).getPropertyValue(style);
+
+
+const initialColors = {
+  bgBody: getStyle(body, "--bg-body"),
+  bgContent: getStyle(body, "--bg-content"),
+  colorText: getStyle(body, "--color-text"),
+}
+
+const darkMode = {
+  bgBody: "#333333",
+  bgContent: "#434343",
+  colorText: "#B5B5B5"
+}
+
+const transformKey = key => "--" + key.replace(/([A-Z])/, "-$1").toLowerCase();
+
+const changeColors = (colors) => {
+  Object.keys(colors).map(key => 
+    body.style.setProperty(transformKey(key), colors[key])
+  )
+}
+
+checkbox.addEventListener("change", ({target}) => {
+  target.checked ? changeColors(darkMode) : changeColors(initialColors);
+})
